d0c3b7f7dac3d949e6971f71a583cc53b504105e
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2025-03-19 15:34:54 +0530

    filter using streams added

--- CoreJavaApp/src/com/corejavaapp/main/controller/EmployeeController.java
+++ CoreJavaApp/src/com/corejavaapp/main/controller/EmployeeController.java
@@ -1,15 +1,35 @@
 package com.corejavaapp.main.controller;
 
 import java.util.List;
-
+ 
 import com.corejavaapp.main.model.Employee;
 import com.corejavaapp.main.service.EmployeeService;
 
 public class EmployeeController {
 
+	EmployeeService employeeService = new EmployeeService();
+	
 	public void displayEmployeeRecord() {
-		EmployeeService employeeService = new EmployeeService();
 		List<Employee> empList =  employeeService.getEmployees();
+		
+		for(Employee e : empList) { //[e1,e2,e3]  e=e1; e=e2; e=e3 
+			System.out.println(e);
+		}
+	}
+ 
+	public void filterByBranch(String ibranch) {
+		List<Employee> empList =  employeeService.getEmployees();
+		
+		empList = employeeService.filterEmployeeByBranch(empList,ibranch);
+		
+		for(Employee e : empList) { //[e1,e2,e3]  e=e1; e=e2; e=e3 
+			System.out.println(e);
+		}
+	}
+
+	public void filterByDepartment(String idepartment) {
+		List<Employee> empList =  employeeService.getEmployees();
+		empList = employeeService.filterEmployeeByDeptartment(empList,idepartment);
 		
 		for(Employee e : empList) { //[e1,e2,e3]  e=e1; e=e2; e=e3 
 			System.out.println(e);
--- CoreJavaApp/src/com/corejavaapp/main/service/EmployeeService.java
+++ CoreJavaApp/src/com/corejavaapp/main/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.corejavaapp.main.service;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import com.corejavaapp.main.model.Employee;
 import com.corejavaapp.main.repository.EmployeeRepository;
@@ -10,6 +11,30 @@ public class EmployeeService {
 	public List<Employee> getEmployees() {
 		EmployeeRepository employeeRepository = new EmployeeRepository();
 		return employeeRepository.getEmployeeList();
+	}
+
+	public List<Employee> filterEmployeeByBranch(List<Employee> empList,String ibranch) {
+		//convert list to stream
+		Stream<Employee> empStream =  empList.parallelStream(); //20 records
+//		try {
+//			Thread.sleep(3000);
+//		} catch (InterruptedException e1) {
+//			e1.printStackTrace();
+//		}
+		//apply the filter
+		empStream = empStream
+						.filter(e-> e.getBranch().equalsIgnoreCase(ibranch)); //this criteria in filter must evaluate to true or false		
+		
+		//recovert to List 
+		empList =  empStream.toList();
+		return empList;
+	}
+
+	public List<Employee> filterEmployeeByDeptartment(List<Employee> empList, String idepartment) {
+		//convert list to stream + apply the filter + recovert to List 
+		return empList.parallelStream()
+					.filter(e->e.getDepartment().equalsIgnoreCase(idepartment))
+					.toList(); 
 	}
 
 }

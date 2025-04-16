a882984e05e8c6c9361bb04a41083868615867bc
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2025-04-16 12:53:26 +0530

    example of states

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -3,42 +3,23 @@ import './App.css'
 
 function App() { //app is a component and a component must have a return 
  
-  let [msg,setMsg] = useState(''); 
-  let [name] = useState('john'); 
-   
-  const age=65;
-
-  //age = 46; 
-  name='emma';
-
-  let x = 10;
-  let y = 5; 
-
-
-  const clickMeFn = ()=>{
-    //alert('I am IN!!!')
-     setMsg("U clicked a button");
-     //console.log(this.msg)
-  }
-
+  const [name,setName] = useState(null);
+  const [email,setEmail] = useState(null);
+  const [contact,setContact] = useState(null); 
   
+  const populate = ()=>{
+    setName("harry potter") 
+    setEmail("harry@gmail.com")
+    setContact("9566985669")
+  }
   return(
     <div>
-      <h1>My React App</h1>
-      <p>Name is {name} </p>
-      <p>Age is {age}</p>
-      <p>Mul is {x*y}</p>
-
-      <hr></hr>
-      <p>
-       {
-          age>60?"You are a Senior Citizen"
-          :age>20?"You are a Middle Age Citizen":"You are a Teenager"
-       }
-      </p>
-      <hr />
-      <button onClick={()=>{ clickMeFn() }}>Click ME!!!</button>
-      <p>{msg}</p>
+      <button onClick={()=>{ populate() }}>Populate Values</button>
+        <p>
+          Name: {name} <br />
+          Email: {email} <br />
+          Contact: {contact} <br />
+        </p>
     </div>
   )
 }

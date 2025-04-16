f0963cede7c408735f15ee93ed3ad432a4873f36
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2025-04-16 12:36:21 +0530

    wrorking with states

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,8 +1,11 @@
+ import { useState } from 'react';
 import './App.css'
 
 function App() { //app is a component and a component must have a return 
  
-  let name='john';
+  let [msg,setMsg] = useState(''); 
+  let [name] = useState('john'); 
+   
   const age=65;
 
   //age = 46; 
@@ -11,6 +14,14 @@ function App() { //app is a component and a component must have a return
   let x = 10;
   let y = 5; 
 
+
+  const clickMeFn = ()=>{
+    //alert('I am IN!!!')
+     setMsg("U clicked a button");
+     //console.log(this.msg)
+  }
+
+  
   return(
     <div>
       <h1>My React App</h1>
@@ -25,8 +36,9 @@ function App() { //app is a component and a component must have a return
           :age>20?"You are a Middle Age Citizen":"You are a Teenager"
        }
       </p>
-     
-      
+      <hr />
+      <button onClick={()=>{ clickMeFn() }}>Click ME!!!</button>
+      <p>{msg}</p>
     </div>
   )
 }

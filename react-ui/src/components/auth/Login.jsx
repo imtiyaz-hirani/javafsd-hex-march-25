6564e37ac925f82929049e1517f949c8df15155c
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2025-04-17 11:55:48 +0530

    login-component-part1

--- react-ui/src/components/auth/Login.jsx
+++ react-ui/src/components/auth/Login.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 function Login(){
+    const [username,setUsername] = useState(null);
+    const [password,setPassword] = useState(null);
+    const [msg,setMsg] = useState(null);
+
+    const login = ()=>{
+        if(username === null){
+            setMsg("Username cannot be blank")
+            return 
+        }
+        if(password == null){
+            setMsg("Password cannot be blank")
+            return 
+        }
+
+        console.log('i am on line 17')
+        alert('alright, u r ready to be tested!!!!')
+    }
+    
     return(
         <div>
             <div className="container-fluid">
+                
                 <div className="row mb-4">
                     <div className="col-lg-12">
                         <nav class="navbar navbar-light bg-light">
@@ -17,21 +38,26 @@ function Login(){
                         
                     </div>
                     <div className="col-sm-4">
-                        <div class="card">
+                        <div className="card">
                             <div className="card-header">
                                 Login
                             </div>
                             <div class="card-body">
                                 <div className="mb-4">
+                                    {msg}
+                                </div>
+                                <div className="mb-4">
                                     <label>Username: </label>
-                                    <input type="text" className="form-control" />
+                                    <input type="text" className="form-control" 
+                                        onChange={$event=>setUsername($event.target.value) } />
                                 </div>
                                 <div className="mb-4">
                                     <label>Password: </label>
-                                    <input type="password" className="form-control"/>
+                                    <input type="password" className="form-control" 
+                                        onChange={($event)=>{setPassword($event.target.value)}}/>
                                 </div>
                                 <div className="mb-4">
-                                <button type="button" class="btn btn-primary">Login</button>
+                                <button type="button" class="btn btn-primary" onClick={()=>{login()}}>Login</button>
                                 </div>
                             </div>
                             <div className="card-footer">
---
+++ react-ui/src/data/users.js
@@ -0,0 +1,40 @@
+const users = [
+    {
+        "id" : 1,
+        "username" : "user1@gmail.com", 
+        "password" : "user1",
+        "role" : "CUSTOMER"
+    }, 
+    {
+        "id" : 2,
+        "username" : "user2@gmail.com", 
+        "password" : "user2",
+        "role" : "CUSTOMER"
+    }, 
+    {
+        "id" : 3,
+        "username" : "vendor1@gmail.com", 
+        "password" : "vendor1",
+        "role" : "VENDOR"
+    }, 
+    {
+        "id" : 4,
+        "username" : "vendor2@gmail.com", 
+        "password" : "vendor2",
+        "role" : "VENDOR"
+    }, 
+    {
+        "id" : 5,
+        "username" : "executive1@gmail.com", 
+        "password" : "executive1",
+        "role" : "EXECUTIVE"
+    },
+    {
+        "id" : 6,
+        "username" : "executive2@gmail.com", 
+        "password" : "executive2",
+        "role" : "EXECUTIVE"
+    }
+]
+
+export default users; 

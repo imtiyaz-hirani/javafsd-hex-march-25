23bf81bc33f50f85e49157b763ffd58e9a276c1b
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2025-04-23 12:29:01 +0530

    get all categories

--- react-ui/src/components/customer/CustomerDashboard.jsx
+++ react-ui/src/components/customer/CustomerDashboard.jsx
@@ -1,7 +1,24 @@
+import { useEffect } from "react";
 import CustomerNavbar from "./navbar";
+import axios from "axios";
+import { useState } from "react";
 
 function CustomerDashboard() {
 
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        const getAllCategories = async () => {
+            try {
+                const response = await axios.get('http://localhost:8081/api/category/all');
+                setCategories(response.data)
+            }
+            catch (err) {
+                console.log(err)
+            }
+        }
+        getAllCategories();
+    }, [])
     return (
         <div className="container-fluid">
             <div className="row">
@@ -12,7 +29,20 @@ function CustomerDashboard() {
 
             <div className="row">
                 <div className="col-lg-12">
-                    ---customer components go here....
+                    <h2>Featured Categories </h2>
+                    <div className="row" >
+                        {
+                            categories.map((c, index) => (
+                                <div className="col-sm-2" key={index}>
+                                    <div className="card">
+                                        <div className="card-body">
+                                            {c.name}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </div>
